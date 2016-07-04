9a3b468583c0474ac5bee63ed67ffe9180b3da51
Damjan <dpavlica@itekako.com>
2016-07-04 13:17:10 +0200

    pokazivaci (2)

--- primeri/pokazivaci.c
+++ primeri/pokazivaci.c
@@ -5,11 +5,10 @@ int main() {
   int bus = 47;
   printf("Varijabla ima vrednost %d i nalazi se na memorijskoj lokaciji %p\n", bus, &bus);
 
-  // skladisti lokaciju varijable tipa int
+  // skladisti lokaciju varijable tipa int u pokazivac
   int * pBus = &bus;
-  printf("Varijabla ima vrednost %d i nalazi se na memorijskoj lokaciji %p\n", bus, pBus);
-
   printf("Pokazivac ima vrednost %p i nalazi se na memorijskoj lokaciji %p\n", pBus, &pBus);
+  printf("Pokazivac pokazuje na varijablu koja ima vrednost %d\n", *pBus);
 
   return 0;
 }
